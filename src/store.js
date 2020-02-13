bc604aab39706fe2c82f5b3ef3f0742590487b07
AaronMullan <aaroncmullan@gmail.com>
2020-02-13 15:08:54 -0800

    masterizer

--- src/store.js
+++
@@ -1,7 +0,0 @@
-const { createStore } = require('redux');
-import reducer from './reducers/moodReducer';
-
-export default createStore(
-  reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
